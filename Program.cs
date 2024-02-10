60ae86e9427bb2033cf41d6f9383fbb0e93094c1
camdyn <camdyn.dev@tutanota.com>
2024-02-09 19:03:30 -0800

    Add PdfPig and CSVHelper

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿/* We have two installed dependencies for this project;
+ * 1. PdfPig - Used for text processing within PDFs
+ * 2. CSVHelper - Used to take Results and aggregate them into a CSV file
+ */
